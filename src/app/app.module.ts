b5beacfd5631fce19851b609d5eb36a019a30ffd
Jeremy Roth <jeremy.roth.heig@gmail.com>
2019-03-10 17:02:09 +0100

    getting to work with graphql

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import {HttpClientModule} from '@angular/common/http';
+import { ApolloModule, APOLLO_OPTIONS } from "apollo-angular";
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { HttpLinkModule, HttpLink } from "apollo-angular-link-http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InMemoryCache } from "apollo-cache-inmemory";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, ApolloModule, HttpLinkModule ],
+  providers: [{
+    provide: APOLLO_OPTIONS,
+    useFactory(httpLink: HttpLink) {
+      return {
+        cache: new InMemoryCache(),
+        link: httpLink.create({
+          uri: "https://tweb-te2-api.herokuapp.com/graphql"
+        })
+      }
+    },
+    deps: [HttpLink]
+  }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Apollo} from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: 'exchange-rates',
+  template: `
+    <div *ngIf="loading">
+      Loading...
+    </div>
+    <div *ngIf="error">
+      Error :(
+    </div>
+    <div *ngIf="users">
+      <div *ngFor="let user of users">
+        <p>{{user.firstname}}</p>
+      </div>
+    </div>
+  `,
 })
-export class Tab1Page {}
+export class Tab1Page implements OnInit {
+  user: any[];
+  loading = true;
+  error: any;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+        users{
+          id
+          firstname
+        }
+        `,
+      })
+      .valueChanges.subscribe(result => {
+        this.user = result.data && result.data.users;
+        this.loading = result.loading;
+        this.error = result.errors;
+      });
+  }
+}
